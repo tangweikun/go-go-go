b7489ab8e8857361427990f4b260c4ad373ffe51
tangweikun <819573105@qq.com>
2018-03-28 13:13:26 +0800

    :sparkles: closures

---
+++ src/closures/closures.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+
+	fmt.Print(nextInt())
+	fmt.Print(nextInt())
+	fmt.Print(nextInt())
+
+	newInts := intSeq()
+	fmt.Print(newInts())
+}
