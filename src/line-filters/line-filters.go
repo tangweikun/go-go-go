232baa420c7375bd23a08900365a4108ae2edc28
tangweikun <819573105@qq.com>
2018-06-11 11:26:34 +0800

    :sparkles: Line Filters

---
+++ src/line-filters/line-filters.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		ucl := strings.ToUpper(scanner.Text())
+		fmt.Println(ucl)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
+
+// cat /tmp/lines | go run "/Users/tangweikun/Playground/go-go-go/src/line-filters/line-filters
+// .go"
